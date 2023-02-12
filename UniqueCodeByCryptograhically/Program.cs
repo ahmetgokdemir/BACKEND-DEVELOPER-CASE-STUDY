f18c7c536701c59e03995fa514cded346f814cbe
ahmetgokdemir <85014820+ahmetgokdemir@users.noreply.github.com>
2023-02-12 21:35:52 +0300

    Add files via upload

---
+++ UniqueCodeByCryptograhically/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+var chars = "ACDEFGHKLMNPRTXYZ234579";
+
+byte[] data = new byte[8];
+byte[] data2 = new byte[8];
+byte[] data3 = new byte[8];
+
+using (RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider())
+{
+    //crypto.GetBytes(data);
+    crypto.GetNonZeroBytes(data);
+    crypto.GetNonZeroBytes(data2);
+    crypto.GetNonZeroBytes(data3);
+
+}
+
+
+StringBuilder result = new StringBuilder(8);
+StringBuilder result2 = new StringBuilder(8);
+StringBuilder result3 = new StringBuilder(8);
+
+foreach (byte b in data)
+{
+    result.Append(chars[b % (chars.Length)]);
+}
+
+foreach (byte b in data2)
+{
+    result2.Append(chars[b % (chars.Length)]);
+}
+
+foreach (byte b in data3)
+{
+    result3.Append(chars[b % (chars.Length)]);
+}
+Console.WriteLine(result.ToString());
+Console.WriteLine(result2.ToString());
+Console.WriteLine(result3.ToString());
+
